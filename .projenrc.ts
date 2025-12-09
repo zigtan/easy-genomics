9c704e5c1dc6fe286597dd51ef62de9af700cdb6
Zig Tan <zigtan@gmail.com>
2025-12-10 00:08:47 +0800

    chore: upgrade pnpm from v9.15.0 to v10.25.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -22,7 +22,7 @@ import { VscodeSettings } from './projenrc/vscode';
 const defaultReleaseBranch = 'main';
 const cdkVersion = '2.176.0';
 const nodeVersion = '20.15.0';
-const pnpmVersion = '9.15.0';
+const pnpmVersion = '10.25.0';
 const authorName = 'DEPT Agency';
 const copyrightOwner = authorName;
 const copyrightPeriod = `${new Date().getFullYear()}`;
